0b71b3cd1cd4769cddf02382d42ee4718f9ecfca
isakon12 <isakedo@gmail.com>
2018-12-25 17:08:36 -0500

    Simplify pragmatic accelerator

--- core/src/BitPragmatic.cpp
+++ core/src/BitPragmatic.cpp
@@ -2,6 +2,7 @@
 #include <core/BitPragmatic.h>
 
 #define N_COLUMNS 16
+#define N_ROWS 16
 #define ZERO_COUNT
 #define BOOTH_ENCODING
 
@@ -11,13 +12,11 @@ namespace core {
     uint8_t BitPragmatic<T>::computePragmaticColumn(int batch, int act_x, int act_y, int kernel_x, int kernel_y,
             int init_channel, int stride, const cnpy::Array<T> &padded_act, int max_channel) {
 
-        // WRONG
-        //Get the maximum number of cycles in this column
+        //Get the slowest neuron in the column
         std::vector<uint8_t> cycles;
-        for(int channel = 0; channel < std::min(16,max_channel); channel++) {
+        for(int channel = init_channel; channel < std::min(init_channel + 16,max_channel); channel++) {
 
-            uint16_t act_bits = padded_act.get(batch, init_channel + channel, stride * act_x + kernel_x,
-                    stride * act_y + kernel_y);
+            uint16_t act_bits = padded_act.get(batch, channel, stride * act_x + kernel_x, stride * act_y + kernel_y);
             #ifdef BOOTH_ENCODING
                 act_bits = this->booth_encoding(act_bits);
             #endif
@@ -45,7 +44,7 @@ namespace core {
             int kernel_x, int kernel_y, int init_channel, int stride, const cnpy::Array<T> &padded_act,
             int max_channel) {
 
-        //Get the number of cycles in the slowest column
+        //Get the slowest column
         std::vector<uint8_t> cycles;
         for(int window = 0; window < list_act_x.size(); window++) {  // Process 16 windows
             uint8_t column_cycles = computePragmaticColumn(batch, list_act_x[window], list_act_y[window], kernel_x,
@@ -74,38 +73,27 @@ namespace core {
         long out_x = (act_shape[2] - wgt_shape[2] + 2*padding)/stride + 1;
         long out_y = (act_shape[3] - wgt_shape[3] + 2*padding)/stride + 1;
 
-        // Set filter grouping
         int groups = (int)act.getShape()[1] / (int)wgt_shape[1];
-        int it_per_group = (int)wgt_shape[0] / groups;
+        auto num_filters_sets = (uint32_t)ceil(wgt_shape[0]/(double)N_ROWS)/groups;
 
         // Convolution
         for(int n=0; n<act_shape[0]; n++) {
-            int current_group = 0, group_m =0, start_group = 0;
             uint32_t cycles = 0;
-            for(int m=0; m<wgt_shape[0]; m += 16) { // Sixteen filters each time
-                std::vector<int> list_x;
-                std::vector<int> list_y;
-                while(this->iterateWindows(out_x,out_y,list_x,list_y,N_COLUMNS)) { // Sixteen windows each time
-                    // Compute in parallel
-                    for (int i = 0; i < Kx; i++) {
-                        for (int j = 0; j < Ky; j++) {
-                            // Sixteen values depthwise, sixteen channels
-                            for (int k = start_group; k < wgt_shape[1] + start_group; k += 16) {
-                                cycles += computePragmaticTile(n, list_x, list_y, i, j, k, stride, padded_act,
-                                        (int)act.getShape()[1]);
-                            }
+            std::vector<int> list_x;
+            std::vector<int> list_y;
+            while(this->iterateWindows(out_x,out_y,list_x,list_y,N_COLUMNS)) { // Sixteen windows each time
+                for (int i = 0; i < Kx; i++) {
+                    for (int j = 0; j < Ky; j++) {
+                        // Sixteen values depthwise, sixteen channels
+                        for (int k = 0; k < act_shape[1]; k += 16) {
+                            cycles += computePragmaticTile(n, list_x, list_y, i, j, k, stride, padded_act,
+                                    (int)act.getShape()[1]);
                         }
                     }
                 }
-                group_m++;
-                if(group_m >= it_per_group) {
-                    group_m = 0;
-                    current_group++;
-                    start_group = (int)wgt_shape[1]*current_group;
-                }
             }
+            std::cout << cycles*num_filters_sets << std::endl;
         }
-
 
     }
 
@@ -135,8 +123,9 @@ namespace core {
                 long out_y = (act_shape[3] - wgt_shape[3] + 2*padding)/stride + 1;
 
                 //Memory stats - 16 bits
-                auto num_weights_sets = (uint32_t)ceil(wgt_shape[0]/16.); // Groups of 16 weights
-                auto num_activations_sets = (uint32_t)ceil(out_x*out_y/16.); // Groups of 16 windows
+                int groups = (int)act.getShape()[1] / (int)wgt_shape[1];
+                auto num_weights_sets = (uint32_t)ceil(wgt_shape[0]/(double)N_ROWS)/groups; // Groups of 16 weights
+                auto num_activations_sets = (uint32_t)ceil(out_x*out_y/(double)N_COLUMNS); // Groups of 16 windows
                 auto num_channel_sets = (uint32_t)ceil(wgt_shape[1]/16.); // Groups of 16 channels
 
                 stats.layers.push_back(layer.getName());
