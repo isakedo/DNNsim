75b065cb513b20f1d19cda28c797ae1d610c3c65
isakon12 <isakedo@gmail.com>
2019-03-22 13:49:31 -0400

    Fix DynamicStripes average width

--- core/src/DynamicStripes.cpp
+++ core/src/DynamicStripes.cpp
@@ -588,7 +588,7 @@ namespace core {
         sys::Statistics::addStats(stats);
     }
 
-    /* AVGERAGE WIDTH */
+    /* AVERAGE WIDTH */
 
     template <typename T>
     typename DynamicStripes<T>::AvgWidth_stats DynamicStripes<T>::computeAvgWidthDynamicStripesTile(int batch,
@@ -686,9 +686,9 @@ namespace core {
         std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
         cnpy::Array<T> act = layer.getActivations();
-        act.powers_of_two_representation();
+        act.sign_magnitude_representation(layer.getAct_precision());
         cnpy::Array<T> wgt = layer.getWeights();
-        wgt.powers_of_two_representation();
+        wgt.sign_magnitude_representation(layer.getWgt_precision());
         if(wgt.getDimensions() == 2) wgt.reshape_to_4D();
 
         int padding = layer.getPadding();
